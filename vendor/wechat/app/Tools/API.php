2af9a9c29be5547d7f03b1600ddaca9162bb7814
fly <fly@load-page.com>
2016-03-08 20:00:36 +0800

    fix

--- vendor/wechat/app/Tools/API.php
+++ vendor/wechat/app/Tools/API.php
@@ -1057,7 +1057,7 @@ class API
 			$this->log($sContent, $url);
 			return $sContent;
 		}else{
-			$this->log(json_encode($aStatus) .PHP_EOL. $sContent, $url);
+			$this->log(json_encode($aStatus). PHP_EOL . curl_errno($oCurl) .':'. curl_error($oCurl) .PHP_EOL. $sContent, $url);
 			return false;
 		}
 	}
