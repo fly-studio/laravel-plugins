72e69fa3609afc4a7f612caed75615bd72445eb6
fly <fly@load-page.com>
2016-08-30 01:32:15 +0800

    upgrade to laravel 5.3

--- vendor/system/routes/web.php
+++ vendor/system/routes/web.php
@@ -1,6 +1,6 @@
 <?php
 
-$router->group(['namespace' => 'Admin','prefix' => 'admin', 'middleware' => ['auth', 'role:admin|manager|owner|leader']], function($router) {
+$router->group(['namespace' => 'Admin','prefix' => 'admin', 'middleware' => ['auth', 'role:admin,manager,owner,leader']], function($router) {
 	
 	$router->addAdminRoutes([
 		'role' => 'RoleController',
