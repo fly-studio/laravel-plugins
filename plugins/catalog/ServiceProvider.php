1d46defd928ed980d2515a50d3aec8567bc9cc5c
fly <fly@load-page.com>
2018-04-20 11:54:36 +0800

    fix catalog's validate

--- plugins/catalog/ServiceProvider.php
+++ plugins/catalog/ServiceProvider.php
@@ -25,7 +25,7 @@ class ServiceProvider extends BaseServiceProvider
 		//添加验证规则
 		Validator::extend('catalog', function($attribute, $value, $parameters, $validator) {
 			foreach ((array)$value as $v) {
-				 $catalogs = catalogs_get_by_id($v);
+				 $catalogs = catalog_search($v);
 				if(empty($catalogs)) return false;
 			}
 			return true;
@@ -37,7 +37,7 @@ class ServiceProvider extends BaseServiceProvider
 
 		Validator::extend('catalog_name', function($attribute, $value, $parameters, $validator) {
 			foreach ((array)$value as $v) {
-				$catalogs = catalogs_get_by_name((!empty($parameters[0]) ? $parameters[0] : '').'.'.$v);
+				$catalogs = catalog_search((!empty($parameters[0]) ? $parameters[0] : '').'.'.$v);
 				if(empty($catalogs)) return false;
 			}
 			return true;
@@ -49,7 +49,7 @@ class ServiceProvider extends BaseServiceProvider
 
 		Validator::extendImplicit('required_if_catalog', function($attribute, $value, $parameters, $validator) {
 			for ($i = 1; $i < count($parameters); $i++) {
-				$catalog = catalogs_get_by_name($parameters[$i]);
+				$catalog = catalog_search($parameters[$i]);
 				$parameters[$i] = !empty($catalog) ? strval($catalog['id']) : $parameters[$i];
 			}
 			return $validator->validateRequiredIf($attribute, $value, $parameters);
@@ -57,7 +57,7 @@ class ServiceProvider extends BaseServiceProvider
 
 		Validator::replacer('required_if_catalog', function($message, $attribute, $rule, $parameters, $validator) {
 			for ($i = 1; $i < count($parameters); $i++) {
-				$catalog = catalogs_get_by_name($parameters[$i]);
+				$catalog = catalog_search($parameters[$i]);
 				$parameters[$i] = !empty($catalog) ? $catalog['title'].'('.$catalog['id'].')' : $parameters[$i];
 			}
 			$parameters[0] = $validator->getAttribute($parameters[0]);
