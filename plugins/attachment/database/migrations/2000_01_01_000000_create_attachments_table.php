d54b6e5633a6298447fb2c5c60e3d24f68021602
fly <fly@load-page.com>
2020-03-28 02:16:12 +0800

    refactor id's type

--- plugins/attachment/database/migrations/2000_01_01_000000_create_attachments_table.php
+++ plugins/attachment/database/migrations/2000_01_01_000000_create_attachments_table.php
@@ -14,11 +14,11 @@ class CreateAttachmentsTable extends Migration
 	public function up()
 	{
 		Schema::create('attachment_files', function (Blueprint $table) {
-			$table->increments('id');
+			$table->id();
 			$table->string('basename', 255)->comment = '存储的文件名';
 			$table->string('path', 255)->comment = '存储文件路径';
 			$table->string('hash', 50)->index()->comment = '文件MD5';
-			$table->unsignedInteger('size')->index()->comment = '文件大小';
+			$table->unsignedBigInteger('size')->index()->comment = '文件大小';
 			$table->timestamp('cdn_at')->nullable()->comment = '上传CDN的时间';
 			$table->timestamps();
 			$table->unique(['hash','size']);
@@ -26,29 +26,30 @@ class CreateAttachmentsTable extends Migration
 		});
 
 		Schema::create('attachments', function (Blueprint $table) {
-			$table->increments('id');
-			$table->unsignedInteger('afid')->default(0)->comment = 'AttachmentFile ID';
+			$table->id();
+			$table->unsignedBigInteger('afid')->default(0)->comment = 'AttachmentFile ID';
+			$table->unsignedBigInteger('thumbnail_aid')->default(0)->comment = 'Thumbnail Attachment ID';
 			$table->string('uuid', 50)->nullable()->unique()->comment = '上传的uuid';
 			$table->unsignedInteger('chunk_count')->default(1)->index()->comment = '块数';
 			$table->string('original_name', 255)->comment = '原始文件名';
 			$table->string('filename', 255)->comment = '新文件名(下载用)';
 			$table->string('ext', 50)->index()->comment = '文件扩展名';
 			$table->json('extra')->nullable()->comment = '其它内容';
-			$table->unsignedInteger('uid')->nullable()->default(0)->comment = '用户ID';
+			$table->unsignedBigInteger('uid')->nullable()->default(0)->comment = '用户ID';
 
 			$table->timestamps(); //创建/修改时间
 		});
 
 		Schema::create('attachment_chunks', function (Blueprint $table) {
-			$table->increments('id');
-			$table->unsignedInteger('aid')->default(0)->comment = 'AttachmentFile ID';
+			$table->id();
+			$table->unsignedBigInteger('aid')->default(0)->comment = 'Attachment ID';
 			$table->unsignedInteger('index')->default(0)->index()->comment = '第几块';
-			$table->unsignedInteger('start')->default(0)->index()->comment = '起始字节';
-			$table->unsignedInteger('end')->default(0)->index()->comment = '结束字节';
+			$table->unsignedBigInteger('start')->default(0)->index()->comment = '起始字节';
+			$table->unsignedBigInteger('end')->default(0)->index()->comment = '结束字节';
 			$table->string('basename', 255)->comment = '存储的文件名(包含扩展名)';
 			$table->string('path', 255)->comment = '存储文件路径';
 			$table->string('hash', 50)->index()->comment = '文件MD5';
-			$table->unsignedInteger('size')->index()->comment = '文件大小';
+			$table->unsignedBigInteger('size')->index()->comment = '文件大小';
 			$table->timestamps();
 			//$table->unique(['hash','size']);
 			$table->softDeletes(); //软删除
--- plugins/catalog/database/migrations/2000_01_01_000000_create_catalog_table.php
+++ plugins/catalog/database/migrations/2000_01_01_000000_create_catalog_table.php
@@ -14,12 +14,12 @@ class CreateCatalogTable extends Migration
 	public function up()
 	{
 		Schema::create('catalogs', function(Blueprint $table) {
-			$table->increments('id');
+			$table->id();
 			$table->string('name', 150)->index()->comment = '英文名称';
 			$table->string('title', 150)->comment = '名称';
 			$table->string('description', 250)->nullable()->comment = '';
 			$table->json('extra')->nullable()->comment = '扩展数据';
-			$table->unsignedInteger('pid')->index()->default(0)->comment = '父ID';
+			$table->unsignedBigInteger('pid')->index()->default(0)->comment = '父ID';
 			$table->unsignedInteger('level')->index()->default(0)->comment = 'tree level';
 			$table->text('path')->nullable()->comment = 'tree path';
 			$table->unsignedInteger('order_index')->default(0)->index()->comment = 'tree order';
